252f6e18e9f665a75bca64f47dbe2ea08d2f64c5
dariaorlova <orlova77m@gmail.com>
2023-11-20 19:19:35 +0300

    Выполнено ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите логин: ");
+        String login = scanner.nextLine();
+        System.out.print("Введите пароль: ");
+        String password = scanner.nextLine();
+        System.out.print("Введите пароль повторно: ");
+        String confirmPassword = scanner.nextLine();
+        try {
+            ValidationService.check(login, password, confirmPassword);
+            System.out.println("Вы успешно вошли в систему");
+        } catch (WrongPasswordException | WrongLoginException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
